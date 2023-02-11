f8e372da313a4d04c88ff201b3128c716713a828
huhu呼呼 <h573980998@gmail.com>
2023-02-11 15:36:27 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -12,47 +12,46 @@ const gamepad: any = computed(() => gamepads.value.find(g => g.mapping === 'stan
 <template>
 	<v-app>
 		<v-main>
-			<v-container style="padding: 0;">
-				<v-row no-gutters justify="center">
-					<v-col class="v-col-xs-12" xs="12" sm="6">
+			<v-row justify="center" class="row">
+				<v-col class="v-col-xs-12" xs="12" sm="6" md="4">
+					<v-sheet>
+						<Gamepad :gamepad="gamepad" />
+					</v-sheet>
+				</v-col>
+				<v-col class="v-col-xs-12" xs="12" sm="6" md="4">
+					<v-sheet>
+						<Mecanum :gamepad="gamepad" :is-orientation="isOrientation" :is-test="isTest" :alpha="alpha" />
+					</v-sheet>
+				</v-col>
+				<transition name="slide-y-transition">
+					<v-col class="v-col-xs-12" xs="12" sm="6" md="4" v-show="isOrientation">
 						<v-sheet>
-							<Gamepad :gamepad="gamepad" />
+							<Orientation v-model="alpha" :is-orientation="isOrientation" />
 						</v-sheet>
 					</v-col>
-					<v-col class="v-col-xs-12" xs="12" sm="6">
-						<v-sheet>
-							<Mecanum :gamepad="gamepad" :is-orientation="isOrientation" :is-test="isTest"
-								:alpha="alpha" />
-						</v-sheet>
-					</v-col>
-					<transition name="slide-y-transition">
-						<v-col class="v-col-xs-12" xs="12" sm="6" v-show="isOrientation">
-							<v-sheet>
-								<Orientation v-model="alpha" :is-orientation="isOrientation" />
-							</v-sheet>
-						</v-col>
-					</transition>
-					<v-col class="v-col-xs-12" xs="12" sm="6">
-						<v-row no-gutters>
-							<v-switch v-model="isOrientation" color="info" hide-details inset
-								label="移动设备躺平在笨笨上确定方向"></v-switch>
-							<v-switch v-model="isTest" color="info" hide-details inset label="测试模式"></v-switch>
-						</v-row>
-					</v-col>
-				</v-row>
-			</v-container>
+				</transition>
+				<v-col class="v-col-xs-12" xs="12" sm="6" md="4">
+					<v-row no-gutters>
+						<v-switch v-model="isOrientation" color="info" hide-details inset
+							label="移动设备躺平在笨笨上确定方向"></v-switch>
+						<v-switch v-model="isTest" color="info" hide-details inset label="测试模式"></v-switch>
+					</v-row>
+				</v-col>
+			</v-row>
 		</v-main>
 	</v-app>
 </template>
 
 <style scoped lang="scss">
-.v-col-xs-12 {
-	.v-sheet {
-		padding: 2px;
-	}
-
-
+.row{
+	max-width: 1600px;
+	margin: 0 auto;
 }
+// .v-col-xs-12 {
+// 	.v-sheet {
+// 		padding: 2px;
+// 	}
+// }
 
 @media (max-width: 600px) {
 	.v-col-xs-12 {
@@ -61,7 +60,7 @@ const gamepad: any = computed(() => gamepads.value.find(g => g.mapping === 'stan
 	}
 }
 
-@media (min-width: 960px) {
+@media (min-width: 1264px) {
 	.v-col-xs-12 {
 		flex: 1 0 0;
 		max-width: 100%;
